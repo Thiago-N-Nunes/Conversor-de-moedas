50f7e1980102d4359d6ed2068c6a4dd810db6921
Thiago Nunes <thiagonesi.cntt@hotmail.com>
2026-05-02 15:59:38 -0300

    Projeto Inicializado

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-palavra = 'Macaco'
-contador = 1
-erro = 0
-print('Jogo da Forca')
-print('_' * len(palavra))
+import os
 
-while erro <= 7:
-    tentativa = input('Digite uma letra:')
-    if tentativa.lower() in palavra.lower():
-        print('ACERTO')
-    else:
-        print('ERRO')
-        erro += 1
-
-    print(f'Numero de Tentativas:{contador}')
-    contador +=1
-    if erro == 7:
-        print('Você perdeu!')
-        break
+print('=' * 30)
+print('CONVERSOR DE MOEDAS')
+print('=' * 30)
+valor = float(input('Digite um valor: '))
+os.system('cls')
+print('=' * 30)
+print('CONVERSÃO')
+print('=' * 30)
+escolha = int(input(f'\n Qual moeda você deseja converter o valor de R$ {valor:.2f}?\n1. Dolar\n2. Euro\n3. Libra\n4. Ienes\n'))
+match escolha:
+    case 1:
+        print(f'O valor de R${valor:.2f} convertido para U${(valor / 4.95):.2f}')
+    case 2:
+        print(f'O valor de R${valor:.2f} convertido para \u20AC{(valor / 5.85):.2f}')
+    case 3:
+        print(f'O valor de R${valor:.2f} convertido para \u00A3{(valor / 6.73):.2f}')
+    case 4:
+         print(f'O valor de R${valor:.2f} convertido para \u00A5{(valor / 0.032):.2f}')
